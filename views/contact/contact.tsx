5c1d962fbc269159196da1370ad91da8275dcc5b
Vikalp Panseriya <vikalppanseriya@gmail.com>
2021-12-04 00:27:14 +0530

    feat: add Calendly link to quick catchup over zoom

--- views/contact/contact.tsx
+++ views/contact/contact.tsx
@@ -2,26 +2,31 @@ import React from 'react'
 import { Spacer, Stack, Text } from '../../components'
 import { Section } from '../section'
 import { ContactForm } from './contact-form/contact-form'
+import { ScheduleCall } from './schedule-call'
 
 export function Contact() {
   return (
     <Section title='Contact'>
       <Spacer size='normal' />
 
-      <Stack fill horizontal='ns'>
+      <Stack className='relative' fill horizontal='ns'>
         <Spacer size='none' ns='large' />
 
         <Stack fill horizontalAlign='center' padding='normal none'>
           <Spacer size='none' ns='normal' />
 
           <Stack fill maxWidth='large'>
-            <Text className='f1-ns f2_5' fontFamily='Righteous'>{`Let's Connect`}</Text>
+            <Stack horizontal='ns'>
+              <Text className='f1-ns f2_5' fontFamily='Righteous'>{`Let's Connect`}</Text>
+              <Spacer size='x-small' ns='large' />
+              <ScheduleCall />
+            </Stack>
             <Spacer ns='large' />
             <ContactForm />
           </Stack>
         </Stack>
-        <Spacer size='small' ns='large' />
 
+        <Spacer size='small' ns='large' />
         <Spacer size='small' ns='none' />
       </Stack>
     </Section>
---
+++ views/contact/schedule-call.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { PopupButton } from 'react-calendly'
+import { COLOR, Spacer, Stack, Text, useTheme } from '../../components'
+import Image from 'next/image'
+
+export function ScheduleCall() {
+  const { getColor } = useTheme()
+  const content = (
+    <Stack horizontal verticalAlign='middle'>
+      <Stack className='flex relative h1_5 w1_5'>
+        <Image layout='fill' src='/images/zoom.png' alt='Zoom' objectFit='contain' />
+      </Stack>
+      <Spacer size='x-small' />
+      <Text className='flex-shrink-0'>Catchup on a call?</Text>
+    </Stack>
+  )
+
+  return (
+    <Stack horizontal verticalAlign='middle'>
+      <PopupButton
+        text={content as any}
+        className='br-pill flex pv2 ph3 pointer ba'
+        styles={{
+          color: getColor(COLOR.SECONDARY),
+          borderColor: '#468DF7',
+          width: 'max-content'
+        }}
+        url='https://calendly.com/vikalppanseriya/20min'
+      />
+    </Stack>
+  )
+}
